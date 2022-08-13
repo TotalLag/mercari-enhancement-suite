575410ecd40cf94dbcebabcc3424fb910ae4ba05
TotalLag <TotalLag@users.noreply.github.com>
2022-08-13 02:08:40 +0000

    Apply automatic changes

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,4 @@
-export const checkVersion: string = "1.21.13";
+export const checkVersion: string = "1.21.14";
 
 export const sleep = (delay: number) =>
   new Promise((resolve) => setTimeout(resolve, delay));
