9839dad9ab2d35253cd875b2c924c9c72bb28b7b
TotalLag <TotalLag@users.noreply.github.com>
2022-09-12 07:01:56 +0000

    Apply automatic changes

--- src/__tests__/app.ts
+++ src/__tests__/app.ts
@@ -1,75 +1,75 @@
-import test from 'tape'
-import cheerio from 'cheerio'
-import { replaceInFile as replace } from 'replace-in-file'
-import { existsSync } from 'fs'
-import { writeFile } from 'fs/promises'
-import { checkVersion } from '../utils'
-const fetch = require('cross-fetch')
+import test from "tape";
+import cheerio from "cheerio";
+import { replaceInFile as replace } from "replace-in-file";
+import { existsSync } from "fs";
+import { writeFile } from "fs/promises";
+import { checkVersion } from "../utils";
+const fetch = require("cross-fetch");
 
 const headers = new fetch.Headers({
-  'User-Agent':
-    'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; FSL 7.0.6.01001)',
-  'Cache-Control': 'no-cache'
-})
+  "User-Agent":
+    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; FSL 7.0.6.01001)",
+  "Cache-Control": "no-cache",
+});
 
-var appVersion: string
-var updated: boolean = false
+var appVersion: string;
+var updated: boolean = false;
 
-test('loads the page and find script url', async t => {
-  const source = await fetch('https://www.mercari.com/sell/', { headers })
-  const html = await source.text()
-  const $ = cheerio.load(html)
-  const scriptTags = $('script[src*=_app]')
-  const appURL = scriptTags.first().attr('src')
-  appVersion = (appURL?.match(/\/*(?:^v)?\d+(?:\.\d+)+/) ?? '')[0]
-  updated = checkVersion !== appVersion
+test("loads the page and find script url", async (t) => {
+  const source = await fetch("https://www.mercari.com/sell/", { headers });
+  const html = await source.text();
+  const $ = cheerio.load(html);
+  const scriptTags = $("script[src*=_app]");
+  const appURL = scriptTags.first().attr("src");
+  appVersion = (appURL?.match(/\/*(?:^v)?\d+(?:\.\d+)+/) ?? "")[0];
+  updated = checkVersion !== appVersion;
 
-  t.assert(appURL !== undefined, 'Page should have asset URL')
-  t.ok(appURL?.includes(appVersion), 'Asset URL should contain version')
+  t.assert(appURL !== undefined, "Page should have asset URL");
+  t.ok(appURL?.includes(appVersion), "Asset URL should contain version");
 
-  const response = await fetch(appURL)
-  const buffer = await response.buffer()
-  await writeFile('./public/js/app.js', buffer)
+  const response = await fetch(appURL);
+  const buffer = await response.buffer();
+  await writeFile("./public/js/app.js", buffer);
 
-  t.ok(existsSync('./public/js/app.js'), 'Downloaded the asset')
-  t.end()
+  t.ok(existsSync("./public/js/app.js"), "Downloaded the asset");
+  t.end();
 
-  test('update version check', { skip: !updated }, async t => {
+  test("update version check", { skip: !updated }, async (t) => {
     const options = {
-      files: './src/utils.ts',
+      files: "./src/utils.ts",
       from: /(const checkVersion: string = ")(.*)(")/g,
-      to: `$1${appVersion}$3`
-    }
+      to: `$1${appVersion}$3`,
+    };
 
-    const results = await replace(options)
-    t.ok(results[0].hasChanged)
-    t.end()
-  })
+    const results = await replace(options);
+    t.ok(results[0].hasChanged);
+    t.end();
+  });
 
-  test('update manifest', { skip: !updated }, async t => {
+  test("update manifest", { skip: !updated }, async (t) => {
     const options = {
-      files: ['./public/manifest.json', './package.json'],
+      files: ["./public/manifest.json", "./package.json"],
       from: /("version": ")(.*)(")/g,
-      to: `$1${appVersion}$3`
-    }
+      to: `$1${appVersion}$3`,
+    };
 
-    const results = await replace(options)
-    t.ok(results[0].hasChanged)
-    t.end()
-  })
-})
+    const results = await replace(options);
+    t.ok(results[0].hasChanged);
+    t.end();
+  });
+});
 
-test('fix blurry images', async t => {
+test("fix blurry images", async (t) => {
   const options = {
-    files: './public/js/app.js',
+    files: "./public/js/app.js",
     from: /(,t.aspect_ratio=s\)}\)\(n,e\);)(const i=\(await u\(n,e,r\)\))/g,
-    to: `$1if(n.width>3272){e.height=Math.floor(3272*n.height/n.width);e.width=3272;}else{e.width=n.width;e.height=n.height;}document.dispatchEvent(new CustomEvent("ping", { detail: { type: "toast", msg: "higher resolution captured ✅" } }));$2`
-  }
+    to: `$1if(n.width>3272){e.height=Math.floor(3272*n.height/n.width);e.width=3272;}else{e.width=n.width;e.height=n.height;}document.dispatchEvent(new CustomEvent("ping", { detail: { type: "toast", msg: "higher resolution captured ✅" } }));$2`,
+  };
 
-  const results = await replace(options)
-  t.ok(results[0].hasChanged)
-  t.end()
-})
+  const results = await replace(options);
+  t.ok(results[0].hasChanged);
+  t.end();
+});
 
 // test("piggyback ready check", async (t) => {
 //   const options = {
