d48fcf5e2f4865095992382db964bdc9790bafb4
TotalLag <phanx068@gmail.com>
2022-09-22 05:05:14 -0500

    update fix

--- src/__tests__/app.ts
+++ src/__tests__/app.ts
@@ -62,8 +62,8 @@ test('loads the page and find script url', async (t) => {
 test('fix blurry images', async (t) => {
   const options = {
     files: './public/js/app.js',
-    from: /(,t.aspect_ratio=s\)}\)\(n,e\);)(const i=\(await u\(n,e,r\)\))/g,
-    to: `$1if(n.height>2880){e.height=2880;e.width=Math.floor(2880*n.width/n.height);if(n.width>3840){e.height=Math.floor(3840*n.height/n.width);e.width=3840;}}else{e.width=n.width;e.height=n.height;}document.dispatchEvent(new CustomEvent("ping", { detail: { type: "toast", msg: "higher resolution captured ✅" } }));$2`,
+    from: /(e.next=5,)(y\(t,i,r\))/g,
+    to: `$1(t.height>2880?(i.height=2880,i.width=Math.floor(2880*t.width/t.height),(t.width>3840?(i.height=Math.floor(3840*t.height/t.width),i.width=3840):'')):(e.width=t.width,i.height=t.height)),document.dispatchEvent(new CustomEvent("ping", { detail: { type: "toast", msg: "higher resolution captured ✅" } })),$2`,
   }
 
   const results = await replace(options)
