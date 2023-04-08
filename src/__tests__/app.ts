c699e439767ae4b1f02fa70fb47ee5b8ef88a35a
TotalLag <phanx068@gmail.com>
2023-04-08 14:57:20 -0500

    fix

--- src/__tests__/app.ts
+++ src/__tests__/app.ts
@@ -62,7 +62,7 @@ test('loads the page and find script url', async (t) => {
 test('fix blurry images', async (t) => {
   const options = {
     files: './public/js/app.js',
-    from: /(f\(i,e\);)(let a=await k\(i,e,r\),o=a.toDataURL\("image\/jpeg"\);)/g,
+    from: /(m\(i,e\);)(let a=await k\(i,e,r\),o=a.toDataURL\("image\/jpeg"\);)/g,
     to: `$1(i.height>2880?(e.height=2880,e.width=Math.floor(2880*i.width/i.height),(i.width>3840??(e.height=Math.floor(3840*i.height/i.width),e.width=3840))):(e.width=i.width,e.height=i.height)),document.dispatchEvent(new CustomEvent("ping", { detail: { type: "toast", msg: "higher resolution captured ✅" } }));$2`,
   }
 
