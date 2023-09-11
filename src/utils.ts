55cf5d7380630b65b303fe352e057cadf92ac65e
matthewaerose <matthewaerose@gmail.com>
2023-09-10 19:04:22 -0500

    Update utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,4 @@
-export const checkVersion: string = '1.26.223'
+export const checkVersion: string = '1.26.234'
 
 export const sleep = (delay: number) =>
   new Promise((resolve) => setTimeout(resolve, delay))
