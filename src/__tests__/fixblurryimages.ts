2e0b7c262c02cb12561024f599f479883a198d60
Chris Phan <TotalLag@users.noreply.github.com>
2024-08-08 09:37:53 -0500

    Update regex in app.ts based on ChatGPT suggestion (#387)

--- src/__tests__/fixblurryimages.ts
+++ src/__tests__/fixblurryimages.ts
@@ -3,7 +3,7 @@ import { replaceInFile as replace } from 'replace-in-file'
 export async function fixBlurryImages() {
   const options = {
     files: './public/js/app.js',
-    from: /(l\(n,e\);)(let r=\(await m\(n,e,i\)\).toDataURL\("image\/jpeg"\);)/g,
+    from: /(l\(n,e\);)(let r=\(await h\(n,e,i\)\).toDataURL\("image\/jpeg"\);)/g,
     to: `$1(n.height>2880?(e.height=2880,e.width=Math.floor(2880*n.width/n.height),(n.width>3840??(e.height=Math.floor(3840*n.height/n.width),e.width=3840))):(e.width=n.width,e.height=n.height)),document.dispatchEvent(new CustomEvent("ping", { detail: { type: "toast", msg: "higher resolution captured ✅" } }));$2`,
   }
 
