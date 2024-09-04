15313f1250b1c966ccfea93845b4fbc890b5dddd
TotalLag <phanx068@gmail.com>
2024-09-04 17:49:35 -0500

    fix tests

--- src/__tests__/fixblurryimages.ts
+++ src/__tests__/fixblurryimages.ts
@@ -3,8 +3,8 @@ import { replaceInFile as replace } from 'replace-in-file'
 export async function fixBlurryImages() {
   const options = {
     files: './public/js/app.js',
-    from: /(l\(n,e\);)(let r=\(await h\(n,e,i\)\).toDataURL\("image\/jpeg"\);)/g,
-    to: `$1(n.height>2880?(e.height=2880,e.width=Math.floor(2880*n.width/n.height),(n.width>3840??(e.height=Math.floor(3840*n.height/n.width),e.width=3840))):(e.width=n.width,e.height=n.height)),document.dispatchEvent(new CustomEvent("ping", { detail: { type: "toast", msg: "higher resolution captured ✅" } }));$2`,
+    from: /(ep\(ei,et\);)(let eu=\(await ek\(ei,et,eo\)\).toDataURL\("image\/jpeg"\);)/g,
+    to: `$1(ei.height>2880?(et.height=2880,et.width=Math.floor(2880*ei.width/ei.height),(ei.width>3840??(et.height=Math.floor(3840*ei.height/ei.width),et.width=3840))):(et.width=ei.width,et.height=ei.height)),document.dispatchEvent(new CustomEvent("ping", { detail: { type: "toast", msg: "higher resolution captured ✅" } }));$2`,
   }
 
   return await replace(options)
